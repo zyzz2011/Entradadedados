804a4de10a901563215393c95f7f7f14cab9bf04
zyzz2011 <169068710+zyzz2011@users.noreply.github.com>
2024-09-05 10:44:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+   //System.out.println("entre com um número inteiro: ");
+//int inteiro = sc.nextInt();
+  //System.out.println("o número digitado foi:");
+  //System.out.println(inteiro);
+  
+
+//System.out.println("entre com um numero racional: ");
+ //double racional = sc.nextDouble();
+    //System.out.println("o número digitado foi:");
+   //System.out.println(racional);
+
+    System.out.println("entre com o nome do seu futuro filho (a) ");
+ String racional = sc.nextDouble();
+    System.out.println("o número digitado foi:");
+    System.out.println(racional);
+  }
+
+  
+}
